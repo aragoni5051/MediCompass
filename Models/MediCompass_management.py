fd6ec998545f793846f86f51f90e62be1d7619bf
KimSeonmo <smsy1230@naver.com>
2024-05-28 00:22:35 +0900

    Frontend 1st edition

---
+++ Models/MediCompass_management.py
@@ -0,0 +1,23 @@
+from typing import Literal, List, Optional, Set
+from datetime import date
+from pydantic import BaseModel, Field, FilePath
+import streamlit as st
+from enum import Enum
+
+
+class InputModel(BaseModel):
+
+    management: str = Field(
+        description="사후 관리를 위한 질문을 입력해 주세요.",
+    )
+    llm_type: Literal['chatgpt', 'huggingface'] = Field(
+        alias='Large Language Model Type',
+        description='사용할 LLM 종류',
+        default='chatgpt',
+    )
+
+
+class OutputModel(BaseModel):
+    suggestion: str = Field(
+        description='사후 관리를 위한 추천'
+    )
